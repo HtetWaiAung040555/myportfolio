773784d611b06d2bde5f1e019be4d5b33a6545ef
HtetWaiAung040555 <htetwai343@gmail.com>
2023-02-10 11:15:53 +0630

    02/10/23 (Update)

--- js/main.js
+++ js/main.js
@@ -8,7 +8,27 @@ $("#home").on('click', function(){
 
 });
 
+$("#homeSm").on('click', function(){
+
+    $("#homeSection").removeClass("d-none");
+    $("#aboutSection").addClass("d-none");
+    $("#contactSection").addClass("d-none");
+    $("#portfolioSection").addClass("d-none");
+    $("#serviceSetion").addClass("d-none");
+
+});
+
 $("#about").on('click', function() {
+    
+    $("#aboutSection").removeClass("d-none");
+    $("#homeSection").addClass("d-none");
+    $("#contactSection").addClass("d-none");
+    $("#portfolioSection").addClass("d-none");
+    $("#serviceSetion").addClass("d-none");
+
+});
+
+$("#aboutSm").on('click', function() {
     
     $("#aboutSection").removeClass("d-none");
     $("#homeSection").addClass("d-none");
@@ -28,6 +48,16 @@ $("#contact").on('click', function() {
 
 });
 
+$("#contactSm").on('click', function() {
+
+    $("#contactSection").removeClass("d-none");
+    $("#aboutSection").addClass("d-none");
+    $("#homeSection").addClass("d-none");
+    $("#portfolioSection").addClass("d-none");
+    $("#serviceSetion").addClass("d-none");
+
+});
+
 $("#portfolio").on('click', function() {
 
     $("#portfolioSection").removeClass("d-none");
@@ -38,7 +68,27 @@ $("#portfolio").on('click', function() {
 
 });
 
+$("#portfolioSm").on('click', function() {
+
+    $("#portfolioSection").removeClass("d-none");
+    $("#contactSection").addClass("d-none");
+    $("#aboutSection").addClass("d-none");
+    $("#homeSection").addClass("d-none");
+    $("#serviceSetion").addClass("d-none");
+
+});
+
 $("#service").on('click', function() {
+
+    $("#serviceSetion").removeClass("d-none");
+    $("#portfolioSection").addClass("d-none");
+    $("#contactSection").addClass("d-none");
+    $("#aboutSection").addClass("d-none");
+    $("#homeSection").addClass("d-none");
+
+});
+
+$("#serviceSm").on('click', function() {
 
     $("#serviceSetion").removeClass("d-none");
     $("#portfolioSection").addClass("d-none");
